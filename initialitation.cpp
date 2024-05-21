0d4ff8da64b2459daedcb6818c3982eab4766d6b
SASCHADDD <saschadanu23@gmail.com>
2024-05-21 17:43:11 +0700

    membenarkan

--- initialitation.cpp
+++ initialitation.cpp
@@ -11,10 +11,16 @@ class mahasiswa {
 public:
     mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai);
     //definisi
-}
 
 mahasiswa() {
     cout << "Id     = " << id << endl;
     cout << "Nama   = " << nama << endl;
-    
+    cout << "Nilai  = " << nilai << endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{12, "sascha", 90,5};
+
+    return 0;
 }
