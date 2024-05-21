0f4226da51446b0504cbdf3c3042cfd6155bf7db
SASCHADDD <saschadanu23@gmail.com>
2024-05-21 19:25:46 +0700

    void

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -10,4 +10,9 @@ class ibu {
         ibu(string pNama) :nama(pNama){
             cout << "Ibu \"" << nama << "\" ada\n";
         }
-}
+        ~ibu(){
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        }
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
