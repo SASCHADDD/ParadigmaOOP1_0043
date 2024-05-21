c8ff238dd72fd726370f062ddd54f1f4c1db4041
SASCHADDD <saschadanu23@gmail.com>
2024-05-21 18:21:42 +0700

    jantung_h

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -5,6 +5,10 @@ using namespace std;
 class jantung{
     public:
         jantung(){
-            
+            cout << "jantung dihidupkan\n";
         }
-}
+        ~jantung(){
+            cout << "jantung dimatikan\n";
+        }
+};
+#endif
